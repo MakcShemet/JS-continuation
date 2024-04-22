75bc9d534046429c5a75522bba8c2d9b4b456d86
DevMasterSh <dev_master_sh@mail.ru>
2024-04-22 19:49:33 +0300

    Solution homework 3

---
+++ JSPluseHomework_3/addreviews.js
@@ -0,0 +1,21 @@
+﻿const submitBtn = document.querySelector('.product__review_btn');
+
+function notEmptyInput(name, review) {
+    return (name === '' || review === '') ? false : true;
+}
+
+submitBtn.addEventListener("click", () => {
+    const productName = document.querySelector('#name-product').value;
+    const textReview = document.querySelector('.product__review_input').value;
+    const errorMessage = document.querySelector('.error-message');
+    const reviewsOnProduct = JSON.parse(localStorage.getItem(productName)) || [];
+
+    if (notEmptyInput(productName, textReview)) {
+        reviewsOnProduct.push(textReview);
+        localStorage.setItem(productName, JSON.stringify(reviewsOnProduct));
+    } else {
+        errorMessage.classList.remove('hidden');
+        errorMessage.textContent = "Поля не должны быть пустыми. Заполните все поля";
+
+    }
+});
---
+++ JSPluseHomework_3/allrevews.js
@@ -0,0 +1,51 @@
+﻿const ulElement = document.querySelector('.list__review');
+
+let keys = Object.keys(localStorage);
+
+keys.forEach(key => {
+    const listElement = document.createElement('li');
+    listElement.classList.add('list__review_item')
+    listElement.textContent = key;
+
+    const vewBtn = document.createElement('details');
+    const summaryElement = document.createElement('summary');
+    summaryElement.textContent = "Открыть";
+
+    vewBtn.addEventListener('click', () => {
+        if (!vewBtn.open) {
+            summaryElement.textContent = "Закрыть";
+        } else {
+            summaryElement.textContent = "Открыть";
+        }
+    });
+
+    let reviewText = JSON.parse(localStorage.getItem(key));
+    reviewText.forEach(reviewElement => {
+        const review = document.createElement('div');
+        review.classList.add('review');
+        review.textContent = reviewElement;
+        vewBtn.appendChild(review);
+
+        const removeReviewBtn = document.createElement('button');
+        removeReviewBtn.classList.add('remove-review');
+        removeReviewBtn.textContent = "Удалить";
+        review.append(removeReviewBtn);
+
+        removeReviewBtn.addEventListener('click', () => {
+            const indexEl = reviewText.indexOf(reviewElement);
+            reviewText.splice(indexEl, 1);
+            if (reviewText.length > 0) {
+                localStorage.setItem(key, JSON.stringify(reviewText));
+            } else {
+                localStorage.removeItem(key);
+            }
+        })
+
+    });
+
+
+
+    ulElement.append(listElement);
+    listElement.append(vewBtn);
+    vewBtn.append(summaryElement);
+})
