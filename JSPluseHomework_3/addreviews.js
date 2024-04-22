694e80d5ef91bab77ab9c569e5cac25aefe3ee4e
DevMasterSh <dev_master_sh@mail.ru>
2024-04-22 21:48:04 +0300

    Solution homework 3

--- JSPluseHomework_3/addreviews.js
+++ JSPluseHomework_3/addreviews.js
@@ -1,21 +1,23 @@
 ﻿const submitBtn = document.querySelector('.product__review_btn');
 
 function notEmptyInput(name, review) {
-    return (name === '' || review === '') ? false : true;
+    return (name.value === '' || review.value === '') ? false : true;
 }
 
 submitBtn.addEventListener("click", () => {
-    const productName = document.querySelector('#name-product').value;
-    const textReview = document.querySelector('.product__review_input').value;
+    const productName = document.querySelector('#name-product');
+    const textReview = document.querySelector('.product__review_input');
     const errorMessage = document.querySelector('.error-message');
     const reviewsOnProduct = JSON.parse(localStorage.getItem(productName)) || [];
 
     if (notEmptyInput(productName, textReview)) {
-        reviewsOnProduct.push(textReview);
-        localStorage.setItem(productName, JSON.stringify(reviewsOnProduct));
+        reviewsOnProduct.push(textReview.value);
+        localStorage.setItem(productName.value, JSON.stringify(reviewsOnProduct));
+        productName.value = '';
+        textReview.value = '';
     } else {
         errorMessage.classList.remove('hidden');
         errorMessage.textContent = "Поля не должны быть пустыми. Заполните все поля";
-
     }
+
 });
