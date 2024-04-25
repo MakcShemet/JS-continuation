09cac6d8ba43a6e94e93bc191014d267b2f5383a
DevMasterSh <dev_master_sh@mail.ru>
2024-04-25 19:50:19 +0300

    Solution homework 1 API browsers

---
+++ API_Homework_1/front.js
@@ -0,0 +1,46 @@
+﻿'use strict'
+
+const sheduleEl = document.querySelector('.shedule');
+
+const shedule = JSON.parse(localStorage.getItem('shedule'));
+console.log(shedule);
+
+shedule.forEach(objTraining => {
+    const trainingEl = document.createElement('div')
+    trainingEl.classList.add('training');
+    trainingEl.id = objTraining.id;
+    sheduleEl.append(trainingEl);
+
+    const titleName = document.createElement('h3');
+    titleName.textContent = objTraining.name;
+    trainingEl.append(titleName);
+
+    const trainingTime = document.createElement('div');
+    trainingTime.classList.add('time-training');
+    trainingTime.textContent = `Время тренировки: ${objTraining.time}`;
+    trainingEl.append(trainingTime);
+
+    const maxParticipantsEl = document.createElement('div');
+    maxParticipantsEl.textContent = `Группа (макс.): ${objTraining.maxParticipants}`;
+    trainingEl.append(maxParticipantsEl);
+
+    const currentParticipantsEl = document.createElement('div');
+    currentParticipantsEl.textContent = `Записалось: ${objTraining.currentParticipants}`;
+    trainingEl.append(currentParticipantsEl);
+
+    const buttonsBlock = document.createElement('div');
+    buttonsBlock.classList.add('buttons-block');
+    trainingEl.append(buttonsBlock);
+
+    const signUpBtn = document.createElement('button');
+    objTraining.currentParticipants === objTraining.maxParticipants ?
+        signUpBtn.classList.add('deactivate') :
+        signUpBtn.classList.add('sign-up');
+    signUpBtn.textContent = 'Записаться';
+    buttonsBlock.append(signUpBtn);
+
+    const cancelBtn = document.createElement('button');
+    cancelBtn.classList.add('cancel');
+    cancelBtn.textContent = 'Отмена';
+    buttonsBlock.append(cancelBtn);
+});
---
+++ API_Homework_1/script.js
@@ -0,0 +1,13 @@
+﻿let countCurrent = 0;
+
+let maxCount = 0;
+
+const signUppButtons = document.querySelectorAll('.sign-up');
+const shedule = JSON.parse(localStorage.getItem('shedule'));
+
+signUppButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const training = button.closest('.training');
+        if (training.id === )
+    })
+});
